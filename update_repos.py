f485a05648424b6e5237660e4b3bc43819c64f78
bcvilnrotter <bcvilnrotter@gmail.com>
2024-10-25 20:45:54 -0400

    update_repos.py script added

---
+++ update_repos.py
@@ -0,0 +1,27 @@
+# %%
+import requests
+
+# Fetch repositories from GitHub API
+response = requests.get("https://api.github.com/users/bcvilnrotter/repos")
+# %%
+repos = response.json()
+# %%
+
+# Start creating an HTML file with repo details
+html_content = "<html><body><h1>Github Repositories</h1></body>"
+for repo in repos:
+    if not repo.get("fork"):
+        html_content += f"""
+            <div>
+                <h3><a href="{repo['html_url']}" target="_blank">{repo['name']}</a></h3>
+                <p>{repo['description']}</p>
+            </div>
+        """
+html_content += "</div></body></html>"
+
+# Write the HTML content to a file
+with open("repos.html","w") as file:
+    file.write(html_content)
+
+# Print the success command to console
+print("HTML file generated successfully!")
