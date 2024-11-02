f2ce388dbaa5881bdd57d2eada62ce2959c6bb35
bcvilnrotter <bcvilnrotter@gmail.com>
2024-11-01 20:00:07 -0400

    poke

--- utils/basic_functions.py
+++ utils/basic_functions.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 # pull secrets when needed
 def get_secret(secret_key):
     # initialization
-    env_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..\..','.gitignore\.env'))
-    load_dotenv(dotenv_path=env_path)
+    if not os.getenv(secret_key):
+        env_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..\..','.gitignore\.env'))
+        load_dotenv(dotenv_path=env_path)
 
     # iteration
     value = os.getenv(secret_key)
